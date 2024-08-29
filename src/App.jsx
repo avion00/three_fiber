3db1b6c657af8ef38f1eaee142dc79176d9294eb
Abhishek kumar chaudhary <avioncompany00@gmail.com>
2024-08-29 21:32:36 +0545

    pus

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
       </EffectComposer>
     </Canvas>
     <div className="w-full bg-zinc-900 py-3">
-      <h1 className="text-zinc-200 p-16 mb-1 text-center">frontend is art and we coders were artist</h1>
+      <h1 className="text-zinc-400 p-16 mb-1 text-center">frontend is art and we coders were artist</h1>
     </div>
     </>
   );
