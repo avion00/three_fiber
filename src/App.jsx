501ca6f89507d3a88677e1c517771a24ca8045b5
Abhishek kumar chaudhary <avioncompany00@gmail.com>
2024-08-31 17:43:02 +0545

    flexaa

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
       </EffectComposer>
     </Canvas>
     <div className="w-full bg-zinc-900 py-3">
-      <h1 className="text-zinc-100 flex p-16 mb-1 text-center">frontend is art and we coders were artist</h1>
+      <h1 className="text-zinc-200 flex p-16 mb-1 text-center">frontend is art and we coders were artist</h1>
     </div>
     </>
   );
